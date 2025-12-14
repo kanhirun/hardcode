20cdd9a06018a4f023d343855e0cf8aca70a01cb
Kel Kanhirun <kel@native.dev>
2025-12-14 08:51:48 -0500

    Adds auto scrolling behavior

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,12 @@ import { Toolbar } from '@/components/toolbar';
 import { StepButton } from '@/components/button';
 import { useMutation } from '@tanstack/react-query';
 import { createDeck } from '@/lib/deck';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import type { Deck } from '@/lib/deck';
 import { CardComponent } from '@/components/Card';
 
 export default function DeckPage() {
+  const [sliceIndex, setSliceIndex] = useState(0);
   const [deck, setDeck] = useState<Deck>({ cards: [] });
   const {
     mutate: createDeckAction,
@@ -19,8 +20,18 @@ export default function DeckPage() {
     },
     onSuccess: (deck => {
       setDeck(deck);
+      setSliceIndex(sliceIndex + 1);
     })
   });
+
+  useEffect(() => {
+    if (sliceIndex > 0) {
+      window.scrollTo({ 
+        top: document.documentElement.scrollHeight, 
+        behavior: 'smooth' 
+      });
+    }
+  }, [sliceIndex]);
 
   return (
     <div className="min-h-screen justify-center">
@@ -32,15 +43,24 @@ export default function DeckPage() {
               JavaScript is a high-level programming language for creating dynamic and interactive web pages. It works with HTML and CSS to enhance user interfaces with features like animations and asynchronous data fetching. JavaScript's frameworks and libraries, such as React and Angular, expand its capabilities in modern web development.
             </p>
             <StepButton 
+              hidden={sliceIndex > 0}
               onClick={() => createDeckAction()}
               className="mt-10"
             >
               Start
             </StepButton>
           </section>
-          { deck.cards.map((card, idx) => (
-              <CardComponent key={idx} card={card} />
-          ))}
+          <section className='flex flex-col gap-10 mb-10'>
+            { deck.cards
+                .slice(0, sliceIndex)
+                .map((card, idx) => (
+                  <CardComponent 
+                    key={idx}
+                    card={card}
+                    onClick={() => setSliceIndex(sliceIndex + 1)}
+                  />
+            ))}
+          </section>
         </main>
     </div>
   );
--- components/Card.tsx
+++ components/Card.tsx
@@ -4,32 +4,39 @@ import { useState } from 'react';
 
 type CardProps = {
   card: Card;
+  onClick: () => void;
   children?: React.ReactNode;
 }
 
 export const CardComponent = ({
   card,
+  onClick: handleClick,
   children
 }: CardProps) => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen)
+    handleClick();
+  }
 
   return (
     <div className='
       flex flex-col items-start gap-4 p-6 rounded
       text-card-foreground bg-card
     '>
-      <div>
+      <code>
         {card.front}
-      </div>
+      </code>
       { !isOpen ?
-          <Button onClick={() => setIsOpen(!isOpen)}>
+          <Button onClick={handleOpen}>
             Show Answer
           </Button> :
           <div className='flex flex-col gap-2'>
             <div>Result:</div>
-            <div>{card.back}</div>
+            <code>{card.back}</code>
           </div>
       }
     </div>
   )
-}
+} 
